4eef3ea7c388f9faf97f3660abdc7f8f75ee1e97
rony-miah <rony.mkt25@gmail.com>
2023-10-02 01:11:58 +0600

    Redux test

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './Counter/CounterSlice';
+
+function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch()
+  
+  return (
+    <>
+      <h3 className='text-2xl'>{count}</h3>
+      <button onClick={() => dispatch(increment())} className='bg-teal-500 p-2 rounded-md mt-5 mr-2 text-white'>Increment</button>
+      <button onClick={() => dispatch(decrement())} className='bg-teal-500 p-2 rounded-md mt-5 text-white'>Decrement</button>
+    </>
+  )
+}
+
+export default App
+
